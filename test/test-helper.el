b26e0145dfbcb1ef415e6710cf8e36bf6bd63f04
Serghei Iakovlev <serghei@phalconphp.com>
2017-07-30 20:11:02 +0300

    Load zephir-mode for tests

--- test/test-helper.el
+++ test/test-helper.el
@@ -28,6 +28,12 @@
 
 ;;; Code:
 
+;; Don't load old byte-compiled versions!
+(setq load-prefer-newer t)
+
+;; Load zephir-mode
+(require 'f)
+(add-to-list 'load-path (f-parent (f-dirname load-file-name)))
 (require 'zephir-mode)
 
 ;;; test-helper.el ends here
