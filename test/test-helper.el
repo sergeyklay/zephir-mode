0b00a33435f26bb0c12d4b1a138397349d4c2d6d
Serghei Iakovlev <serghei@phalconphp.com>
2017-08-07 08:23:40 +0300

    Bump version

--- test/test-helper.el
+++ test/test-helper.el
@@ -4,7 +4,7 @@
 
 ;; Author: Serghei Iakovlev (serghei@phalconphp.com)
 ;; Maintainer: Serghei Iakovlev
-;; Version: 0.3.3
+;; Version: 0.3.4
 ;; URL: https://github.com/sergeyklay/zephir-mode
 
 ;; This file is not part of GNU Emacs.
